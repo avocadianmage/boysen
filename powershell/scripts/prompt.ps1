fcc3df3a1980cc5131ea5cd4d03a22432e8f8c5e
avocadianmage <claytongotwals@gmail.com>
2018-08-20 19:25:28 -0500

    move to 3/4 bit ansi color codes, since xterm.js supports them

--- powershell/scripts/prompt.ps1
+++ powershell/scripts/prompt.ps1
@@ -1,24 +1,28 @@
 # Specify prompt format.
 function prompt
 {
+    # ANSI codes for 3/4 bit terminal color.
+    $yellow = 33
+    $brightGreen = 92
+
+    # Prefix the prompt with an indicator if currently elevated.
+    if (isElevated) { $adminPrefix = getColoredString "[admin] " $yellow }
+
     $workingDirectory = $ExecutionContext.SessionState.Path.CurrentLocation
-    $workingDirectory = getColoredString $workingDirectory (135, 255, 135)
-    if (isContextElevated) 
-    { 
-        $adminPrefix = getColoredString "[admin] " (255, 175, 0)
-    }
+    $workingDirectory = getColoredString $workingDirectory $brightGreen
+
     "$adminPrefix$workingDirectory$('$' * ($NestedPromptLevel + 1)) "
 }
 
 # Returns text of the specified color, build with ANSI escape sequences.
-function getColoredString($text, $rgb)
+function getColoredString($text, $ansiColor)
 {
     $ESC = [char]27
-    "$ESC[38;2;$($rgb[0]);$($rgb[1]);$($rgb[2])m$text$ESC[0m"
+    "$ESC[$($ansiColor)m$text$ESC[0m"
 }
 
 # Determines if the currently running script has administrative privileges.
-function isContextElevated
+function isElevated
 {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent()
     $userSecurity = New-Object Security.Principal.WindowsPrincipal $user
