bf5c09d30a8c1575791be43fb27efec7cdd98753
avocadianmage <claytongotwals@gmail.com>
2018-08-26 00:42:28 -0500

    update console title with cwd in preparation for tabs

--- powershell/scripts/prompt.ps1
+++ powershell/scripts/prompt.ps1
@@ -1,3 +1,7 @@
+# ANSI escape characters.
+$ESC = [char]27;
+$BEL = [char]7;
+
 # Specify prompt format.
 function prompt
 {
@@ -8,17 +12,27 @@ function prompt
     # Prefix the prompt with an indicator if currently elevated.
     if (isElevated) { $adminPrefix = getColoredString "[admin] " $yellow }
 
-    $workingDirectory = $ExecutionContext.SessionState.Path.CurrentLocation
-    $workingDirectory = getColoredString $workingDirectory $brightGreen
+    # Get current working directory.
+    $cwd = $ExecutionContext.SessionState.Path.CurrentLocation
+    $coloredCWD = getColoredString $cwd $brightGreen
 
-    "$adminPrefix$workingDirectory$('$' * ($NestedPromptLevel + 1)) "
+    # Ensure the console title is updated with the current working directory.
+    $windowTitle = setWindowTitle $cwd
+
+    # Build and return prompt.
+    "$windowTitle$adminPrefix$coloredCWD$('$' * ($NestedPromptLevel + 1)) "
 }
 
 # Returns text of the specified color, build with ANSI escape sequences.
 function getColoredString($text, $ansiColor)
 {
-    $ESC = [char]27
     "$ESC[$($ansiColor)m$text$ESC[0m"
+}
+
+# Uses the appropriate ANSI escape sequence to set the window.
+function setWindowTitle($title)
+{
+    "$ESC]2;$title$BEL"
 }
 
 # Determines if the currently running script has administrative privileges.
